313988c137758aee4cc5cd69d9ba5fdfc3d2684c
Derek J. Russell <djamesr23@gmail.com>
2021-03-21 20:31:39 -0700

    pairsThatEqualSum Solution (UCP)

--- main.py
+++ main.py
@@ -1,54 +1,33 @@
-# Name: Derek J. Russell
+# Name Derek J. Russell
 # Course: Uber Career Prep
 # HW Assignment 1: Arrays & Strings (Part 2)
-# Date: March 21st, 2021
+# Date March 21st, 2021
 
-# Time Complexity: O(n Log n)
+# Time Complexity: O(n^2)
 
-def isStringPermutation(s1: str, s2: str) -> bool:
+def pairsThatEqualSum(inputArray: list, targetSum: int) -> list:
     """
-    Implement the function isStringPermutation() that takes two Strings
-    as parameters and returns a Boolean denoting whether the first
-    string is a permutation of the second string.
+     Implement the function pairsThatEqualSum() that takes an array of
+     integers and a target integer and returns an array of pairs (i.e.,
+     an array of tuples) where each pair contains two numbers from the
+     input array and the sum of each pair equals the target integer.
     """
-
     """
-    Get lengths of both strings. If the length of both strings
-    is not the same, then they cannot be Permutation.
+     The 1st for-loop loops for each index of inputArray. The 2nd for-loop 
+     loops for index from i+1 to end of inputArray. Next, it checks the sum 
+     of elements at indexes (k, j) matches the sum. After that, it adds the 
+     values at indexes k and j to answer. Lastly, it returns the answer. 
     """
-    if len(s1) != len(s2):
-        return False
+    answer = []
 
-    # Sort both strings.
-    temp1 = sorted(s1)
-    s1 = " ".join(temp1)
-    temp2 = sorted(s2)
-    s2 = " ".join(temp2)
-
-    # Compare the sorted strings.
-    for j in range(0, len(s1), 1):
-        if s1[j] != s2[j]:
-            return False
-    return True
+    for k in range(len(inputArray)):
+        for j in range(k + 1, len(inputArray)):
+            if inputArray[k] + inputArray[j] == targetSum:
+                answer.append((inputArray[k], inputArray[j]))
+    return answer
 
 
-# Driver Code
-if __name__ == '__main__':
-    s1, s2 = "asdf", "fsda"
-    s3, s4 = "asdf", "fsa"
-    s5, s6 = "asdf", "fsax"
-
-    if isStringPermutation(s1, s2):
-        print("True")
-    else:
-        print("False")
-
-    if isStringPermutation(s3, s4):
-        print("True")
-    else:
-        print("False")
-
-    if isStringPermutation(s5, s6):
-        print("True")
-    else:
-        print("False")
+# Driver Code - (Testing).
+print(pairsThatEqualSum([1, 2, 3, 4, 5], 5))
+print(pairsThatEqualSum([1, 2, 3, 4, 5], 1))
+print(pairsThatEqualSum([1, 2, 3, 4, 5], 7))
